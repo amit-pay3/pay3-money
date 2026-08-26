cc217e5eeb5b0611df330308def9c3ec72ea9adf
Amit P <amit@pay3.money>
2026-08-27 00:28:02 +0530

    removed unwanted config files

--- src/pages/api/keystatic/[...params].ts
+++
@@ -1,5 +0,0 @@
-import { makeGenericHandler } from '@keystatic/core/api/generic';
-import { createHandler } from '@keystatic/astro/api';
-import config from '../../../keystatic.config';
-
-export const all = createHandler(makeGenericHandler(config));
