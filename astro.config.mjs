dbfb38226c86bdd3cf4840ae9c5df06d44e93059
Amit P <amit@pay3.money>
2026-08-27 00:27:33 +0530

    added server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), keystatic()],
+  adapter: node({ mode: 'standalone' }),
 });
